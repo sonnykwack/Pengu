61457bc7e232b1efda9c17c9bdbda68f32cf159f
sonnykwack <159227008+sonnykwack@users.noreply.github.com>
2026-07-06 19:46:11 +0900

    Delete penguproject directory

--- penguproject/mobile/android/app/src/main/kotlin/com/pengucare/pengu_care/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.pengucare.pengu_care
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- penguproject/mobile/lib/models/fall_event.dart
+++
@@ -1,6 +0,0 @@
-class FallEvent {
-  final DateTime detectedAt;
-  final double peakMagnitude;
-
-  FallEvent({required this.detectedAt, required this.peakMagnitude});
-}
